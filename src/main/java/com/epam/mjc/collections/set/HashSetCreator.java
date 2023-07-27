91d08696ed0e13ba2105380221057a83dfa9ca76
ruslanmerdeev <r.merdeev@haulmont.com>
2023-07-27 12:13:59 +0400

    [main] Filled up the HashSetCreator.java

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(Integer current : sourceList) {
+            if (current % 2 == 0) {
+                int i = current;
+                for (; i % 2 == 0; i /= 2) {
+                    set.add(i);
+                }
+                set.add(i);
+            } else {
+                set.add(current);
+                set.add(current*2);
+            }
+        }
+
+        return set;
     }
 }
